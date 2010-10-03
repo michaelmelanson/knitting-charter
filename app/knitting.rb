d8e442a556fdf5c2bea53703ded140490aea020c
Michael Melanson <michael@michaelmelanson.net>
2010-10-03 13:43:49 -0400

    Optimized cache controls and ordering of external resources

--- app/knitting.rb
+++ app/knitting.rb
@@ -4,6 +4,21 @@ require 'base64'
 require 'haml'
 require 'sass'
 
+class Fixnum
+  def minutes
+    self * 60
+  end
+  def hours
+    (self * 60).minutes
+  end
+  def days
+    (self * 24).hours
+  end
+  def year
+    (self * 365).days
+  end
+end
+
 class KnittingApp < Sinatra::Base
   configure :production do
       ENV['APP_ROOT'] ||= File.dirname(__FILE__)
@@ -15,10 +30,12 @@ class KnittingApp < Sinatra::Base
   set :public, "public"
 
   get '/' do
+    cache_control :public, :max_age => 6.hours
     haml :index
   end
 
   get '/style.css' do
+    cache_control :public, :max_age => 6.hours
     content_type 'text/css', :charset => 'utf-8'
     sass :style
   end
@@ -26,13 +43,24 @@ class KnittingApp < Sinatra::Base
   get '/chart.json' do
     instructions = params[:instructions].strip.split(/[\r\n]+/)
     hashtag = Base64.encode64 params[:instructions]
+    puts "instructions: #{instructions}"
+    if instructions.empty?
+      chart = [""]
+    else
+      chart = Chart.from_instructions(instructions).to_text
+    end
 
-    { :chart => Chart.from_instructions(instructions).to_text,
+    puts "chart: #{chart}"
+
+    cache_control :public, :max_age => 1.year
+    { :chart => chart,
       :hashtag => hashtag.strip }.to_json
   end
 
   get '/chart/load.json' do
     encoded = params[:encoded]
+
+    cache_control :public, :max_age => 1.year
     { :instructions => Base64.decode64(encoded) }.to_json
   end
 end
